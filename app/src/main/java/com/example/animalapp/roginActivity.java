e46bc6ea289cc85254900b167d95fbe66696a09d
dlwpg <ljhun605@jbnu.ac.kr>
2025-03-31 15:39:07 +0900

    [Add] define xml to java

--- app/src/main/java/com/example/animalapp/roginActivity.java
+++ app/src/main/java/com/example/animalapp/roginActivity.java
@@ -1,6 +1,9 @@
 package com.example.animalapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class roginActivity extends AppCompatActivity {
 
+    EditText editText1, editText2;
+    Button btn;
+    TextView textView;
+    String str1, str2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_rogin);
+
+        editText1 = findViewById(R.id.editID);
+        editText2 = findViewById(R.id.editPD);
+
+        btn = findViewById(R.id.loginbutton);
+        textView = findViewById(R.id.signin);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
